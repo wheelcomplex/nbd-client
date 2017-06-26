5111c22fd376468b0550d6d62405bcf61b51b601
Ryan Moeller <ryan@freqlabs.com>
2017-06-26 01:05:51 -0700

    Make loop state handlers return the next state
    This removes the loop state from the loop context struct, as well.
    
    Includes a few bonus whitespace fixes.

--- main.c
+++ main.c
@@ -94,13 +94,12 @@ struct loop_context {
 	uint8_t *buf;
 	size_t buflen;
 	int result;
-	enum loop_state state;
 };
 
-static inline void
-init_ctx(struct loop_context *ctx,
-		 ggate_context_t ggate,
-		 nbd_client_t nbd)
+static inline enum loop_state
+loop_init(struct loop_context *ctx,
+	  ggate_context_t ggate,
+	  nbd_client_t nbd)
 {
 
 	ctx->ggate = ggate;
@@ -111,12 +110,13 @@ init_ctx(struct loop_context *ctx,
 	};
 	ctx->buf = NULL;
 	ctx->buflen = 0;
-	ctx->state = SETUP;
+
+	return SETUP;
 }
 
-static inline void
+static inline enum loop_state
 loop_setup(struct loop_context *ctx,
-		   uint8_t *buf, size_t buflen)
+	   uint8_t *buf, size_t buflen)
 {
 
 	ctx->buf = buf;
@@ -124,7 +124,8 @@ loop_setup(struct loop_context *ctx,
 	ctx->ggio.gctl_data = buf;
 	ctx->ggio.gctl_length = buflen;
 	ctx->ggio.gctl_error = 0;
-	ctx->state = START;
+
+	return START;
 }
 
 static inline int
@@ -134,7 +135,7 @@ ggioctl(struct loop_context *ctx, uint64_t req)
 	return ggate_context_ioctl(ctx->ggate, req, &ctx->ggio);
 }
 
-static inline void
+static inline enum loop_state
 loop_start(struct loop_context *ctx)
 {
 	int result;
@@ -142,25 +143,21 @@ loop_start(struct loop_context *ctx)
 	result = ggioctl(ctx, G_GATE_CMD_START);
 
 	if (result == FAILURE) {
-		ctx->state = FAIL;
+		return FAIL;
 	}
-	else {
-		switch (ctx->ggio.gctl_error) {
-		case SUCCESS:
-			ctx->state = DO_CMD;
-			break;
 
-		case ECANCELED:
-			ctx->state = FINISHED;
-			break;
+	switch (ctx->ggio.gctl_error) {
+	case SUCCESS:
+		return DO_CMD;
 
-		case ENXIO:
-		default:
-			syslog(LOG_ERR, "%s: ggate control operation failed: %s",
-				   __func__, strerror(ctx->ggio.gctl_error));
-			ctx->state = FAIL;
-			break;
-		}
+	case ECANCELED:
+		return FINISHED;
+
+	case ENXIO:
+	default:
+		syslog(LOG_ERR, "%s: ggate control operation failed: %s",
+		       __func__, strerror(ctx->ggio.gctl_error));
+		return FAIL;
 	}
 }
 
@@ -171,34 +168,34 @@ nbdcmd(struct loop_context *ctx)
 	switch (ctx->ggio.gctl_cmd) {
 	case BIO_READ:
 		return nbd_client_send_read(ctx->nbd,
-									ctx->ggio.gctl_seq,
-									ctx->ggio.gctl_offset,
-									ctx->ggio.gctl_length);
+					    ctx->ggio.gctl_seq,
+					    ctx->ggio.gctl_offset,
+					    ctx->ggio.gctl_length);
 
 	case BIO_WRITE:
 		return nbd_client_send_write(ctx->nbd,
-									 ctx->ggio.gctl_seq,
-									 ctx->ggio.gctl_offset,
-									 ctx->ggio.gctl_length,
-									 ctx->buflen, ctx->buf);
+					     ctx->ggio.gctl_seq,
+					     ctx->ggio.gctl_offset,
+					     ctx->ggio.gctl_length,
+					     ctx->buflen, ctx->buf);
 
 	case BIO_DELETE:
 		return nbd_client_send_trim(ctx->nbd,
-									ctx->ggio.gctl_seq,
-									ctx->ggio.gctl_offset,
-									ctx->ggio.gctl_length);
+					    ctx->ggio.gctl_seq,
+					    ctx->ggio.gctl_offset,
+					    ctx->ggio.gctl_length);
 
 	case BIO_FLUSH:
 		return nbd_client_send_flush(ctx->nbd, ctx->ggio.gctl_seq);
 
 	default:
 		syslog(LOG_NOTICE, "%s: unsupported operation: %d",
-			   __func__, ctx->ggio.gctl_cmd);
+		       __func__, ctx->ggio.gctl_cmd);
 		return EOPNOTSUPP;
 	}
 }
 
-static inline void
+static inline enum loop_state
 loop_command(struct loop_context *ctx)
 {
 	int result;
@@ -207,28 +204,24 @@ loop_command(struct loop_context *ctx)
 
 	switch (result) {
 	case SUCCESS:
-		ctx->state = RECV_HEADER;
-		break;
+		return RECV_HEADER;
 
 	case EOPNOTSUPP:
 		ctx->ggio.gctl_error = EOPNOTSUPP;
-		ctx->state = END_CMD;
-		break;
+		return END_CMD;
 
 	case FAILURE:
 		syslog(LOG_ERR, "%s: nbd client error", __func__);
-		ctx->state = FAIL;
-		break;
+		return FAIL;
 
 	default:
 		syslog(LOG_ERR, "%s: unhandled nbd command result: %d",
-			   __func__, result);
-		ctx->state = FAIL;
-		break;
+		       __func__, result);
+		return FAIL;
 	}
 }
 
-static inline void
+static inline enum loop_state
 hdrinval(struct loop_context* ctx)
 {
 	char const *name;
@@ -237,28 +230,29 @@ hdrinval(struct loop_context* ctx)
 		// Some servers lie about support for TRIM.
 		nbd_client_disable_trim(ctx->nbd);
 		ctx->ggio.gctl_error = EOPNOTSUPP;
-		ctx->state = END_CMD;
+
+		return END_CMD;
 	}
-	else {
-		syslog(LOG_ERR, "%s: server rejected command request", __func__);
 
-		name = bio_cmd_string(ctx->ggio.gctl_cmd);
+	syslog(LOG_ERR, "%s: server rejected command request", __func__);
 
-		if (name == NULL)
-			syslog(LOG_DEBUG, "\tcommand: %u (unknown)", ctx->ggio.gctl_cmd);
-		else
-			syslog(LOG_DEBUG, "\tcommand: %s", name);
+	name = bio_cmd_string(ctx->ggio.gctl_cmd);
 
-		syslog(LOG_DEBUG, "\toffset: %lx (%ld)",
-			   ctx->ggio.gctl_offset, ctx->ggio.gctl_offset);
-		syslog(LOG_DEBUG, "\tlength: %lx (%lu)",
-			   ctx->ggio.gctl_length, ctx->ggio.gctl_length);
+	if (name == NULL)
+		syslog(LOG_DEBUG, "\tcommand: %u (unknown)",
+		       ctx->ggio.gctl_cmd);
+	else
+		syslog(LOG_DEBUG, "\tcommand: %s", name);
 
-		ctx->state = FAIL;
-	}
+	syslog(LOG_DEBUG, "\toffset: %lx (%ld)",
+	       ctx->ggio.gctl_offset, ctx->ggio.gctl_offset);
+	syslog(LOG_DEBUG, "\tlength: %lx (%lu)",
+	       ctx->ggio.gctl_length, ctx->ggio.gctl_length);
+
+	return FAIL;
 }
 
-static inline void
+static inline enum loop_state
 loop_recv_header(struct loop_context* ctx)
 {
 	int result;
@@ -267,49 +261,48 @@ loop_recv_header(struct loop_context* ctx)
 
 	switch (result) {
 	case SUCCESS:
-		ctx->state = (ctx->ggio.gctl_cmd == BIO_READ) ? RECV_DATA : END_CMD;
-		break;
+		return (ctx->ggio.gctl_cmd == BIO_READ) ? RECV_DATA : END_CMD;
 
 	case EINVAL:
-		hdrinval(ctx);
-		break;
+		return hdrinval(ctx);
 
 	default:
 		if (disconnect) {
-			ctx->state = FINISHED;
+			return FINISHED;
 		}
 		else {
-			syslog(LOG_ERR, "%s: error receiving reply header", __func__);
-			ctx->state = FAIL;
+			syslog(LOG_ERR, "%s: error receiving reply header",
+			       __func__);
+			return FAIL;
 		}
-		break;
 	}
 }
 
-static inline void
+static inline enum loop_state
 loop_recv_data(struct loop_context *ctx)
 {
 	int result;
 
 	result = nbd_client_recv_reply_data(ctx->nbd,
-										ctx->ggio.gctl_length,
-										ctx->buflen, ctx->buf);
+					    ctx->ggio.gctl_length,
+					    ctx->buflen, ctx->buf);
 
 	if (result == FAILURE) {
 		if (disconnect) {
-			ctx->state = FINISHED;
+			return FINISHED;
 		}
 		else {
-			syslog(LOG_ERR, "%s: error receiving reply data", __func__);
-			ctx->state = FAIL;
+			syslog(LOG_ERR, "%s: error receiving reply data",
+			       __func__);
+			return FAIL;
 		}
 	}
 	else {
-		ctx->state = END_CMD;
+		return END_CMD;
 	}
 }
 
-static inline void
+static inline enum loop_state
 loop_end_command(struct loop_context *ctx)
 {
 	int result;
@@ -318,26 +311,22 @@ loop_end_command(struct loop_context *ctx)
 
 	if (result == FAILURE) {
 		syslog(LOG_ERR, "%s: could not complete transaction", __func__);
-		ctx->state = FAIL;
+		return FAIL;
 	}
-	else {
-		switch (ctx->ggio.gctl_error) {
-		case SUCCESS:
-		case EOPNOTSUPP:
-			ctx->state = SETUP;
-			break;
 
-		case ECANCELED:
-			ctx->state = FINISHED;
-			break;
+	switch (ctx->ggio.gctl_error) {
+	case SUCCESS:
+	case EOPNOTSUPP:
+		return SETUP;
 
-		case ENXIO:
-		default:
-			syslog(LOG_ERR, "%s: ggate control operation failed: %s",
-				   __func__, strerror(ctx->ggio.gctl_error));
-			ctx->state = FAIL;
-			break;
-		}
+	case ECANCELED:
+		return FINISHED;
+
+	case ENXIO:
+	default:
+		syslog(LOG_ERR, "%s: ggate control operation failed: %s",
+		       __func__, strerror(ctx->ggio.gctl_error));
+		return FAIL;
 	}
 }
 
@@ -347,16 +336,19 @@ run_loop(ggate_context_t ggate, nbd_client_t nbd)
 	struct sigaction sa;
 	uint8_t buf[MAXPHYS];
 	struct loop_context context;
-	struct loop_context *ctx = &context;
+	struct loop_context *ctx;
+	enum loop_state current_state;
 
 	sa.sa_sigaction = signal_handler;
 	sa.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGINT, &sa, NULL) == FAILURE) {
-		syslog(LOG_ERR, "%s: failed to install signal handler: %m", __func__);
+		syslog(LOG_ERR, "%s: failed to install signal handler: %m",
+		       __func__);
 		return FAILURE;
 	}
 
-	init_ctx(ctx, ggate, nbd);
+	ctx = &context;
+	current_state = loop_init(ctx, ggate, nbd);
 
 	for (;;) {
 		if (disconnect) {
@@ -365,29 +357,29 @@ run_loop(ggate_context_t ggate, nbd_client_t nbd)
 			break;
 		}
 
-		switch (context.state) {
+		switch (current_state) {
 		case SETUP:
-			loop_setup(ctx, &buf[0], sizeof buf);
+			current_state = loop_setup(ctx, &buf[0], sizeof buf);
 			break;
 
 		case START:
-			loop_start(ctx);
+			current_state = loop_start(ctx);
 			break;
 
 		case DO_CMD:
-			loop_command(ctx);
+			current_state = loop_command(ctx);
 			break;
 
 		case RECV_HEADER:
-			loop_recv_header(ctx);
+			current_state = loop_recv_header(ctx);
 			break;
 
 		case RECV_DATA:
-			loop_recv_data(ctx);
+			current_state = loop_recv_data(ctx);
 			break;
 
 		case END_CMD:
-			loop_end_command(ctx);
+			current_state = loop_end_command(ctx);
 			break;
 
 		case FINISHED:
